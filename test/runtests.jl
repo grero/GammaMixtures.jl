8665b5e9586773bec03e8ad7d56a0a514bf81614
Roger Herikstad <roger.herikstad@gmail.com>
2024-09-27 10:18:02 +0800

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,25 +9,26 @@ using StableRNGs
     model = GammaMixture([1.0, 10.0],[1.0, 1.0], [0.2, 0.8])
     x = rand(rng, model, 500)
 
+    ll0 = loglikelihood(model, x)
+    @test ll0 ≈ -1355.7955874052238
+
     #test initializers
     t0 = time()
     model10 = GammaMixture(2, x, MOMInitializer)
     Δt = time() - t0
-    @show Δt
+    ll1 = loglikelihood(model10, x)
 
     t0 = time()
     model1,Δl = fit!(model10, x;niter=10_000)
     Δt = time() - t0
     @test model1.converged
-    @show model1 Δl Δt
 
     t0 = time()
     model20 = GammaMixture(2, x, NaiveInitializer)
     Δt = time() - t0
-    @show Δt
 
     t0 = time()
     model2,Δl = fit!(model20, x;niter=10_000)
+    @test model2.converged
     Δt = time() - t0
-    @show model2 Δl Δt
 end
